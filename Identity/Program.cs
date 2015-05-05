4f57b9248e571785c3b78e9234a3dec900015620
Herman Gouw <hermangouw@gmail.com>
2015-05-06 07:10:11 +1000

    Added Identity

---
+++ Identity/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Identity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetUserName());
+            Console.ReadLine();
+        }
+
+        static string GetUserName()
+        {
+            return String.Format("{0}\\{1}", Environment.UserDomainName, Environment.UserName); ;
+        }
+    }
+}
