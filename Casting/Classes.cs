ed1a5e66bdcf7e4dfd21f30fb2a559f9f85e7860
Herman Gouw <hermangouw@gmail.com>
2015-05-16 16:38:57 +1000

    Added Casting

---
+++ Casting/Classes.cs
@@ -0,0 +1,17 @@
+﻿namespace Casting
+{
+    public class Asset
+    {
+        public string Name { get; set; }
+    }
+
+    public class House : Asset
+    {
+        public decimal Price { get; set; }
+    }
+
+    public class Stock : Asset
+    {
+        public long SharesOwned { get; set; }
+    }
+}
---
+++ Casting/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            House house = new House { Name = "37 Robert Road, Cherrybrook", Price = 1000000 };
+            Console.WriteLine("house -> ({0}, {1})", house.Name, house.Price);
+
+            Stock stock = new Stock { Name = "Computershare", SharesOwned = 6000 };
+            Console.WriteLine("stock -> ({0}, {1})", stock.Name, stock.SharesOwned);
+
+            Console.ReadLine();
+
+            Asset asset = stock; // Implicitly upcast to a base class reference
+            Console.WriteLine("(asset == stock) -> {0}", asset == stock);
+            Console.WriteLine("asset -> ({0})", asset.Name);
+
+            Stock cpu = (Stock)asset; // Explicitly downcast to a subclass reference
+            Console.WriteLine("google -> ({0}, {1})", cpu.Name, cpu.SharesOwned);
+
+            House mansion;
+            try
+            {
+                mansion = (House)asset;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("mansion = (House)asset -> ({0}", ex.Message);
+            }
+
+            mansion = asset as House;
+            Console.WriteLine("mansion = asset as House -> ({0})", mansion == null ? "null" : "NOT null");
+
+            Console.ReadLine();
+        }
+    }
+}
