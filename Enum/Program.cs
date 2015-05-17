6d04e4e435be72c21f5a2267c0fc2ce1c9b3f50d
Herman Gouw <hermangouw@gmail.com>
2015-05-17 14:52:17 +1000

    Added more tests

---
+++ Enum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    class Program
+    {
+        [Flags]
+        enum Direction
+        {
+            None = 0,
+            Left = 1,
+            Right = 2,
+            Top = 4,
+            Bottom = 8,
+            Vertical = Left | Right,
+            Horizontal = Top | Bottom,
+            All = Vertical | Horizontal
+        }
+
+        static void Main(string[] args)
+        {
+            Direction direction = Direction.Top | Direction.Right;
+            Console.WriteLine(direction.ToString());
+            Console.WriteLine(Direction.Left.ToString());
+            Console.WriteLine(Direction.Horizontal.ToString());
+            Console.WriteLine(Direction.Vertical.ToString());
+            Console.WriteLine(Direction.All.ToString());
+            Console.ReadLine();
+        }
+    }
+}
