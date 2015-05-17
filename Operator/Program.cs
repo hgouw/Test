8597851b9907df41230d2604d28c5e80f571c9aa
Herman Gouw <hermangouw@gmail.com>
2015-05-17 10:57:30 +1000

    Added more tests

--- Operator/Program.cs
+++ Operator/Program.cs
@@ -14,6 +14,17 @@ namespace Operator
             a += a++;
             Console.WriteLine("a = " + a);
             Console.ReadLine();
+
+            int x = 20, y = 35;
+            x = x++ + y++; // x = 55, y = 36
+            y = ++x + ++y; // x = 56, x = 93
+            Console.WriteLine("{0}{1}", x, y);
+            Console.ReadLine();
+
+            var s = "Hello World!";
+            //s = 6; // Once a var has been assigned a type, it can no longer be changed
+            Console.WriteLine(s);
+            Console.ReadLine();
         }
     }
 }
