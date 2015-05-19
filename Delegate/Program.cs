2e9598f026c4b0685e7591a9d089355078baae1a
Herman Gouw <hermangouw@gmail.com>
2015-05-19 11:53:25 +1000

    Added Delegate

---
+++ Delegate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class Program
+    {
+        public delegate void WriteDelegate(string msg);
+
+        static void Main(string[] args)
+        {
+            Log log = new Log();
+            WriteDelegate writeDelegate = new WriteDelegate(log.Write);
+            writeDelegate("Hello World!");
+            Console.ReadLine();
+        }
+    }
+
+    public class Log
+    {
+        public void Write(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
