60e7d998ca116ca87a910df35ff2b3bf5b229970
Herman Gouw <hermangouw@gmail.com>
2015-05-21 19:38:01 +1000

    More changes

---
+++ Subscribe/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Subscribe
+{
+    class Program
+    {
+        public class MarketData
+        {
+            public decimal Price { get; set; }
+            public void Update (decimal price)
+            {
+                Price = price;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // TODO - object interested in MarketData price changes can subscribe to an update event
+        }
+    }
+}
