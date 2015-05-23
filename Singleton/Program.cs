95406d8b6c7ed7d7ae9ef0a909f67e4cfb7bbdca
Herman Gouw <hermangouw@gmail.com>
2015-05-24 07:55:39 +1000

    More changes

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -7,20 +7,35 @@
         }
     }
 
-    public class Singleton
+    public class Singleton1
     {
-        private static Singleton instance;
+        private static Singleton1 instance;
 
-        private Singleton() { }
+        private Singleton1() { }
 
-        public static Singleton Instance
+        public static Singleton1 Instance
         {
             get
             {
                 if (instance == null)
                 {
-                    instance = new Singleton();
+                    instance = new Singleton1();
                 }
+                return instance;
+            }
+        }
+    }
+
+    public sealed class Singleton2
+    {
+        private static readonly Singleton2 instance = new Singleton2();
+
+        private Singleton2() { }
+
+        public static Singleton2 Instance
+        {
+            get
+            {
                 return instance;
             }
         }
