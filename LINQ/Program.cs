8dbe1c6c286a3a38d7da1e895a7b143dcb4994fe
Herman Gouw <hermangouw@gmail.com>
2015-05-26 08:57:55 +1000

    Added LINQ

---
+++ LINQ/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timesheets = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            var authorizationRequests = new int[] { 2, 4, 5, 7, 9, 11 };
+            var selectedTimesheets = timesheets.Where(x => authorizationRequests.Contains(x)).ToList();
+            foreach (int i in selectedTimesheets)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
