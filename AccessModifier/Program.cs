858920631f5b605aab0d9ef97dc67125b34da303
Herman Gouw <hermangouw@gmail.com>
2015-06-11 12:04:17 +1000

    Added AccessModifier

---
+++ AccessModifier/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AccessModifier
+{
+    enum Enumeration { enum1, enum2, enum3 } // Property is public by default
+
+    class Class // Constructor is internal by default
+    {
+        public Class() { } // Constructor is private by default
+        public int field; // Field is private by default
+        public int Property { get; set; } // Property is private by default
+        public void Method() { } // Property is private by default
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class cls = new Class();
+            cls.field = 0;
+            cls.Property = 0;
+            cls.Method();
+        }
+    }
+}
