df6ff4cf78616c542826e8d12a3367352fe745ae
Herman Gouw <hermangouw@gmail.com>
2015-06-16 15:22:19 +1000

    Lock

---
+++ Lock/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lock
+{
+    class Program
+    {
+        private static object syncRoot = new Object();
+
+        static void Main(string[] args)
+        {
+            lock (syncRoot)
+            {
+                lock (syncRoot)
+                {
+                    Console.Write("Hello ");
+                }
+                Console.WriteLine("World!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
