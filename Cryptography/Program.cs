a533f91580dd59a63a34fae3efb3d99d5b018f27
Herman Gouw <hermangouw@gmail.com>
2015-06-16 15:21:44 +1000

    Added Cryptography

---
+++ Cryptography/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Cryptography
+{
+    class Program
+    {
+        static string CreateSHA1(string value)
+        {
+            SHA1 algorithm = SHA1.Create();
+            byte[] data = algorithm.ComputeHash(Encoding.UTF8.GetBytes(value));
+            string sha1 = "";
+            for (int i = 0; i < data.Length; i++)
+            {
+                sha1 += data[i].ToString("x2").ToUpperInvariant();
+            }
+            return sha1;
+        }
+
+        static void Main(string[] args)
+        {
+            var passwords = new string[] { "J3sus", "M05e5", "D@v1d", "@6r@h@m" };
+            foreach (string password in passwords)
+            {
+                Console.WriteLine(String.Format("{0} - {1}", password, CreateSHA1(password)));
+            }
+            Console.ReadLine();
+        }
+    }
+}
