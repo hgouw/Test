fba7d0b97407977d8e3754735d6de8aa89e117e7
Herman Gouw <hermangouw@gmail.com>
2015-06-17 16:54:21 +1000

    More changes

--- Casting/Program.cs
+++ Casting/Program.cs
@@ -20,19 +20,19 @@ namespace Casting
         static void Main(string[] args)
         {
             House house = new House { Name = "37 Robert Road, Cherrybrook", Price = 1000000 };
-            Console.WriteLine("house -> ({0}, {1})", house.Name, house.Price);
+            Console.WriteLine("House -> ({0}, {1})", house.Name, house.Price);
 
             Stock stock = new Stock { Name = "Computershare", SharesOwned = 6000 };
-            Console.WriteLine("stock -> ({0}, {1})", stock.Name, stock.SharesOwned);
+            Console.WriteLine("Stock -> ({0}, {1})", stock.Name, stock.SharesOwned);
 
             Console.ReadLine();
 
             Asset asset = stock; // Implicitly upcast to a base class reference
             Console.WriteLine("(asset == stock) -> {0}", asset == stock);
-            Console.WriteLine("asset -> ({0})", asset.Name);
+            Console.WriteLine("Asset -> ({0})", asset.Name);
 
             Stock cpu = (Stock)asset; // Explicitly downcast to a subclass reference
-            Console.WriteLine("google -> ({0}, {1})", cpu.Name, cpu.SharesOwned);
+            Console.WriteLine("Stock -> ({0}, {1})", cpu.Name, cpu.SharesOwned);
 
             House mansion;
             try
