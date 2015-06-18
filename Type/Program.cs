21ec78907a998a075a8f2dd7dd5c40e3e5dd6543
Herman Gouw <hermangouw@gmail.com>
2015-06-18 15:24:43 +1000

    Added Type

---
+++ Type/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 6;
+            var f = 6.10;
+
+            Console.WriteLine(i.GetType().Name);
+            Console.WriteLine(i.GetType().FullName);
+            Console.WriteLine(typeof(int));
+            Console.WriteLine(f.GetType().Name);
+            Console.WriteLine(f.GetType().FullName);
+            Console.WriteLine(typeof(double));
+
+            Console.ReadLine();
+        }
+    }
+}
