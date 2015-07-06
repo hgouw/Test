891886df8592948d04492abe74c17d1c9ff33a05
Herman Gouw <hermangouw@gmail.com>
2015-07-07 00:04:14 +1000

    Added Collections

---
+++ Collections/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Collections
+{
+    class User
+    {
+        public string Type { get; set; }
+        public User(string type)
+        {
+            Type = type;
+        }
+    }
+    class Program
+    {
+        static void Main(params string[] args)
+        {
+            Dictionary<string, User> users = new Dictionary<string, User>()
+            {
+                {"admin", new User("admin")},
+                {"guest", new User("guest")}
+            };
+        }
+    }
+}
