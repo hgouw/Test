ca52c2b7ccdf96a58c610c06e44f913ceb37e19e
Herman Gouw <hermangouw@gmail.com>
2015-07-07 17:21:26 +1000

    Added Format

---
+++ Format/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Format
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 1024;
+            Console.WriteLine(String.Format("{0:E}", (object)value));
+            Console.WriteLine(String.Format("{0,00000000:X}", (object)value));
+            Console.WriteLine(String.Format("{0:000}", (object)value));
+            Console.ReadLine();
+
+            Console.ReadLine();
+        }
+    }
+}
