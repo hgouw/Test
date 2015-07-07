0d0832ef3c25a2d0051ebd77cdda7f0ac0ece42a
Herman Gouw <hermangouw@gmail.com>
2015-07-07 21:33:56 +1000

    Added Polymorphism

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        class Pet
+        {
+            public virtual void Speak()
+            {
+                Console.WriteLine("Not Implemented");
+            }
+        }
+        class Cat : Pet
+        {
+            public override void Speak()
+            {
+                Console.WriteLine("Meow");
+            }
+        }
+        class Dog : Pet
+        {
+            public new void Speak()
+            {
+                Console.WriteLine("Woof");
+            }
+        }
+        class Bird : Pet
+        {
+            public void Speak()
+            {
+                Console.WriteLine("Tweet");
+            }
+        }
+        static void Main()
+        {
+            List<Pet> pets = new List<Pet>();
+            pets.Add(new Cat());
+            pets.Add(new Dog());
+            pets.Add(new Bird());
+            foreach (var pet in pets)
+                pet.Speak();
+            Console.ReadLine();
+        }
+    }
+}
