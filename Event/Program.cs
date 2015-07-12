9b05817b8865f8c4234810f844d6bb2e912b7554
Herman Gouw <hermangouw@gmail.com>
2015-07-13 02:59:05 +1000

    Added Event

---
+++ Event/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace Event
+{
+    public class Program
+    {
+        static ManualResetEvent MyEvent;
+
+        public static void Main()
+        {
+            MyEvent = new ManualResetEvent(true);
+
+            ThreadStart MyThreadStart = new ThreadStart(Worker);
+            Thread MyThread = new Thread(MyThreadStart);
+            MyThread.Start();
+
+            Console.ReadKey();
+        }
+
+        static void Worker()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Working ...");
+                MyEvent.WaitOne();
+            }
+        }
+    }
+}
