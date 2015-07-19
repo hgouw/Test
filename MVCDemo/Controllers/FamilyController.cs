ac80e38b430d7567e7914c1f9128ecc231447a88
Herman Gouw <hermangouw@gmail.com>
2015-07-19 10:05:48 +1000

    Added MVCDemo

---
+++ MVCDemo/Controllers/FamilyController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class FamilyController : Controller
+    {
+        /// <summary>
+        /// To display this page type in http://localhost/MVCDemo/Family/Members
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Members()
+        {
+            ViewBag.Members =
+                new List<string>
+                {
+                    "Herman",
+                    "Helen",
+                    "Sarah",
+                    "Olivia"
+                };
+            return View();
+        }
+
+    }
+}
---
+++ MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// To invoke this page type in http://localhost/MVCDemo/Home/Home/xx?querystr1=yy&querystr2=zz
+        /// where id, querystr1 and querystr2 are optional
+        /// </summary>
+        public string Home(string id, string querystr1, string querystr2)
+        {
+            var output = "Hello World!";
+            if (!String.IsNullOrEmpty(id))
+            {
+                output = String.Format("Id - {0}", id);
+                if (!String.IsNullOrEmpty(querystr1))
+                {
+                    output = String.Format("{0} - {1}", output, querystr1);
+                    if (!String.IsNullOrEmpty(querystr2))
+                    {
+                        output = String.Format("{0} - {1}", output, querystr2);
+                    }
+                }
+            }
+            return output;
+        }
+    }
+}
