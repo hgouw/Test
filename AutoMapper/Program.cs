cd5658e009896218dde959367bbaff08d54a038b
Herman Gouw <hermangouw@gmail.com>
2015-09-07 14:21:49 +1000

    Added AutoMapper

---
+++ AutoMapper/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace AutoMapper
+{
+    public class Customer
+    {
+        public Company Company { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int NumberOfOrders { get; set; }
+        public bool VIP { get; set; }
+    }
+
+    public class CustomerViewItem
+    {
+        public string CompanyName { get; set; }
+        public string FullName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DateOfBirth { get; set; }
+        public int NumberOfOrders { get; set; }
+        public string VIP { get; set; }
+    }
+
+    public class Company
+    {
+        public string Name { get; set; }
+    }
+
+    public class DateFormatter : IValueFormatter
+    {
+        public string FormatValue(ResolutionContext context)
+        {
+            return ((DateTime)context.SourceValue).ToLongDateString();
+        }
+    }
+
+    public class VIPResolver : ValueResolver<bool, string>
+    {
+        protected override string ResolveCore(bool source)
+        {
+            return source ? "Y" : "N";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            Mapper.CreateMap<Customer, CustomerViewItem>()
+                .ForMember(cv => cv.FullName, m => m.MapFrom(s => s.FirstName + " " + s.LastName))
+                .ForMember(cv => cv.VIP, m => m.ResolveUsing<VIPResolver>().FromMember(x => x.VIP))
+                .ForMember(cv => cv.DateOfBirth, m => m.AddFormatter<DateFormatter>());
+
+            program.Run();
+        }
+
+        private void Run()
+        {
+            Customer customer = GetCustomerFromDB();
+
+            CustomerViewItem customerViewItem = Mapper.Map<Customer, CustomerViewItem>(customer);
+
+            ShowCustomerInDataGrid(customerViewItem);
+        }
+
+        private Customer GetCustomerFromDB()
+        {
+            return new Customer()
+            {
+                DateOfBirth = new DateTime(1987, 11, 2),
+                FirstName = "Andriy",
+                LastName = "Buday",
+                NumberOfOrders = 7,
+                Company = new Company() { Name = "andriybuday.blogspot.com" },
+                VIP = true
+            };
+        }
+
+        private void ShowCustomerInDataGrid(CustomerViewItem customerViewItem) { }
+    }
+}
