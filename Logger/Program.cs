40ae8a5dd6835c9132daa78d2519164306861a83
Herman Gouw <hermangouw@gmail.com>
2015-09-12 03:50:47 +1000

    Added Logger

---
+++ Logger/Program.cs
@@ -0,0 +1,16 @@
+﻿using log4net;
+using log4net.Config;
+
+namespace Logger
+{
+    class Program
+    {
+        private static readonly ILog Log = LogManager.GetLogger("Test.Logger");
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
+            Log.Debug("Application is starting");
+            Log.Debug("Application is ending");
+        }
+    }
+}
