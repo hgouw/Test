0d90bb8600b411b3a29d8e9d771dfce81202d67e
Herman Gouw <hermangouw@gmail.com>
2016-04-08 16:24:37 +1000

    Added ASX project

---
+++ ASX/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace ASX
+{
+    public class Program
+    {
+        static void Main()
+        {
+            using (var context = new ASXContext())
+            {
+                var companies = context.Companies.ToList();
+            }
+        }
+    }
+
+    public class ASXContext : DbContext
+    {
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<IndustryGroup> IndustryGroups { get; set; }
+    }
+
+    public class IndustryGroup
+    {
+        [Key]
+        public string Group { get; set; }
+        public List<Company> Companies { get; set; }
+    }
+
+    public class Company
+    {
+        [Key]
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public List<EndOfDay> EndOfDays { get; set; }
+    }
+
+    public class EndOfDay
+    {
+        [Key, Column(Order = 1), ForeignKey("Company")]
+        public string Code { get; set; }
+        [Key, Column(Order = 2)]
+        public DateTime Date { get; set; }
+        public decimal Open { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public decimal Close { get; set; }
+        public int Volume { get; set; }
+        public Company Company { get; set; }
+    }
+}
