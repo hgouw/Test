0594b2ccce9a1696b730a2346f8a4a667cfd71c0
Herman Gouw <hermangouw@gmail.com>
2016-06-09 14:29:06 +1000

    Added Product

--- CSV/Program.cs
+++ CSV/Program.cs
@@ -20,6 +20,12 @@ namespace CSV
         public virtual int Volume { get; set; }
     }
 
+    public class Product
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+
     public sealed class EndOfDayMap : CsvClassMap<EndOfDay>
     {
         public EndOfDayMap()
@@ -33,37 +39,52 @@ namespace CSV
         }
     }
 
+    public sealed class ProductMap : CsvClassMap<Product>
+    {
+        public ProductMap()
+        {
+            Map(m => m.ProductId).Index(0);
+            Map(m => m.ProductName).Index(1);
+        }
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            StreamReader csvReader = File.OpenText(@"..\..\Data\Test.csv");
+            //StreamReader csvReader = File.OpenText(@"..\..\Data\Test.csv");
+            //using (var parser = new CsvParser(csvReader))
+            //{
+            //    while (true)
+            //    {
+            //        var row = parser.Read();
+            //        if (row == null)
+            //        {
+            //            break;
+            //        }
+            //    }
+            //}
+            //using (var csv = new CsvReader(csvReader))
+            //{
+            //    csv.Configuration.RegisterClassMap<EndOfDayMap>();
+            //    while (csv.Read())
+            //    {
+            //        var record = csv.GetRecord<EndOfDay>();
+            //    }
+            //}
+            //using (var csv = new CsvReader(csvReader))
+            //{
+            //    csv.Configuration.RegisterClassMap<EndOfDayMap>();
+            //    var records = csv.GetRecords<EndOfDay>().ToList();
+            //}
 
-            using (var parser = new CsvParser(csvReader))
-            {
-                while (true)
-                {
-                    var row = parser.Read();
-                    if (row == null)
-                    {
-                        break;
-                    }
-                }
-            }
-
+            StreamReader csvReader = File.OpenText(@"..\..\Data\DL_BM_Products_Export.csv");
             using (var csv = new CsvReader(csvReader))
             {
-                csv.Configuration.RegisterClassMap<EndOfDayMap>();
-                while (csv.Read())
-                {
-                    var record = csv.GetRecord<EndOfDay>();
-                }
-            }
-
-            using (var csv = new CsvReader(csvReader))
-            {
-                csv.Configuration.RegisterClassMap<EndOfDayMap>();
-                var records = csv.GetRecords<EndOfDay>().ToList();
+                csv.Configuration.HasHeaderRecord = false;
+                csv.Configuration.RegisterClassMap<ProductMap>();
+                var products = csv.GetRecords<Product>().ToList();
+                var uniqueproducts = products.Distinct();
             }
         }
     }
