439aaa4fdc860621af1bebe781a422a6ef60909b
Herman Gouw <hermangouw@gmail.com>
2016-06-16 16:22:31 +1000

    Refactored

--- Sort/CustomSort.cs
+++ Sort/CustomSort.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Sort
 {
@@ -10,42 +7,37 @@ namespace Sort
     {
         int IComparer<string>.Compare(string s1, string s2)
         {
+            int ret = 0;
+
             char c1 = s1[0];
             char c2 = s2[0];
 
-            switch (c1)
+            if (char.IsNumber(c1))
             {
-                case 'g':
-                    if (c2 == 'p')
-                    {
-                        return -1;
-                    }
-                    else
-                    {
-                        goto default;
-                    }
-                case 'r':
-                    if (c2 == 'p' || c2 == 'g')
-                    {
-                        return -1;
-                    }
-                    else
-                    {
-                        goto default;
-                    }
-                case 'j':
-                    if (c2 == 'p' || c2 == 'g' || c2 == 'r')
-                    {
-                        return -1;
-                    }
-                    else
-                    {
-                        goto default;
-                    }
-                default:
-                    int ret = (c2 != c1) ? 1 : string.Compare(s1, s2, StringComparison.Ordinal);
-                    return ret;
+                if (char.IsNumber(c2))
+                {
+                    //-1 (c1 < c2)
+                    //0  (c1 = c2)
+                    //1  (c1 > c2)
+                }
+                else
+                {
+
+                }
             }
+            else
+            {
+                if (char.IsNumber(c2))
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+
+            return ret;
         }
     }
 
