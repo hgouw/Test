3dce199d7b72616309b4402eba3b40cfad83fd1f
Herman Gouw <hermangouw@gmail.com>
2016-06-16 15:39:36 +1000

    Refactored

--- Sort/CustomSort.cs
+++ Sort/CustomSort.cs
@@ -10,13 +10,13 @@ namespace Sort
     {
         int IComparer<string>.Compare(string s1, string s2)
         {
-            char l1 = s1[0];
-            char l2 = s2[0];
+            char c1 = s1[0];
+            char c2 = s2[0];
 
-            switch (l1)
+            switch (c1)
             {
                 case 'g':
-                    if (l2 == 'p')
+                    if (c2 == 'p')
                     {
                         return -1;
                     }
@@ -25,7 +25,7 @@ namespace Sort
                         goto default;
                     }
                 case 'r':
-                    if (l2 == 'p' || l2 == 'g')
+                    if (c2 == 'p' || c2 == 'g')
                     {
                         return -1;
                     }
@@ -34,7 +34,7 @@ namespace Sort
                         goto default;
                     }
                 case 'j':
-                    if (l2 == 'p' || l2 == 'g' || l2 == 'r')
+                    if (c2 == 'p' || c2 == 'g' || c2 == 'r')
                     {
                         return -1;
                     }
@@ -43,71 +43,9 @@ namespace Sort
                         goto default;
                     }
                 default:
-                    int ret = (l2 != l1) ? 1 : string.Compare(s1, s2, StringComparison.Ordinal);
+                    int ret = (c2 != c1) ? 1 : string.Compare(s1, s2, StringComparison.Ordinal);
                     return ret;
             }
         }
-
-        //public int IComparer<string>.Compare(string s1, string s2)
-        //{
-        //    char l1 = s1[0];
-        //    char l2 = s2[0];
-        //    int ret;
-
-        //    switch (l1)
-        //    {
-        //        case 'g':
-        //            if (l2 == 'p')
-        //            {
-        //                return -1;
-        //            }
-        //            else
-        //            {
-        //                goto default;
-        //            }
-        //        case 'r':
-        //            if (l2 == 'p' || l2 == 'g')
-        //            {
-        //                return -1;
-        //            }
-        //            else
-        //            {
-        //                goto default;
-        //            }
-        //        case 'j':
-        //            if (l2 == 'p' || l2 == 'g' || l2 == 'r')
-        //            {
-        //                return -1;
-        //            }
-        //            else
-        //            {
-        //                goto default;
-        //            }
-        //        default: ret = (l2 != l1) ? 1 : s1.CompareTo(s2); return ret;
-        //    }
-        //    return ret;
-        //}
-
-        //public int Compare(string x, string y)
-        //{
-        //    int ix = x == "b" ? 0 : x == "c" ? 1 : 2;
-        //    int iy = y == "b" ? 0 : y == "c" ? 1 : 2;
-        //    return ix.CompareTo(iy);
-        //}
-
-        //public int Compare(string x, string y)
-        //{
-        //    //todo null checks on input
-        //    var pairs = x.Zip(y, (a, b) => new { x = a, y = b });
-        //    foreach (var pair in pairs)
-        //    {
-        //        int value = pair.x.CompareTo(pair.y);
-        //        if (value != 0)
-        //            return value;
-        //    }
-        //    //if we got here then either they are the same,
-        //    //or one starts with the other
-        //    return y.Length.CompareTo(x.Length); //note x and y are reversed here
-        //}
     }
 }
