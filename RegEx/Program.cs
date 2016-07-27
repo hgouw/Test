9214814e062deda579be2509e957963d7b718b81
Herman Gouw <hermangouw@gmail.com>
2016-07-27 12:39:52 +1000

    Added RegEx project

---
+++ RegEx/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var oldString = @"<p><a href=""https://www.legislation.gov.au/Details/C2011A00137"">Work Health and Safety Act 2011 (Cth)</a></p>";
+            Console.WriteLine(oldString);
+            var pattern = @"(?<start><a[^>]*)(?<end>>)";
+            var replacement = @"${start} target=""_blank"" ${end}";
+            var newString = Regex.Replace(oldString, pattern, replacement);
+            Console.WriteLine(newString);
+            Console.ReadLine();
+        }
+    }
+}
